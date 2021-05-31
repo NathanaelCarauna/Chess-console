05fc84573bf294677deeb214b41949d65473af30
NathanaelCarauna <Nathanael.carauna@gmail.com>
2021-05-31 09:55:35 -0300

    namespace renamed

---
+++ Xadrez-console/Tabuleiro/Peca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        private Tabuleiro tab;
+    }
+}
---
+++ Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+    }
+}
