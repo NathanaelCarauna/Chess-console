c1b11ca7b2135be99f586faf279b2fd810968e5c
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-06-01 18:27:43 -0300

    Peao implemented

---
+++ Xadrez-console/xadrez/Peao.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Peao : Peca
+    {
+        public Peao(Cor cor, Tabuleiro tabuleiro) : base(cor, tabuleiro)
+        {
+        }
+
+        private bool ExisteInimigo(Posicao pos)
+        {
+            Peca p = Tabuleiro.Peca(pos);
+            return p != null && p.Cor != Cor;
+        }
+
+        private bool Livre(Posicao pos)
+        {
+            return Tabuleiro.Peca(pos) == null;
+        }
+
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+            if (Cor == Cor.Branca)
+            {
+                pos.SetPosicao(Posicao.Linha - 1, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && Livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha - 2, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && Livre(pos) && QteMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha - 1, Posicao.Coluna - 1);
+                if (Tabuleiro.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha - 1, Posicao.Coluna + 1);
+                if (Tabuleiro.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+
+            }
+            else
+            {
+                pos.SetPosicao(Posicao.Linha + 1, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && Livre(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha + 2, Posicao.Coluna);
+                if (Tabuleiro.PosicaoValida(pos) && Livre(pos) && QteMovimentos == 0)
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha + 1, Posicao.Coluna - 1);
+                if (Tabuleiro.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+                pos.SetPosicao(Posicao.Linha + 1, Posicao.Coluna + 1);
+                if (Tabuleiro.PosicaoValida(pos) && ExisteInimigo(pos))
+                {
+                    mat[pos.Linha, pos.Coluna] = true;
+                }
+            }
+
+            return mat;
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+    }
+}
