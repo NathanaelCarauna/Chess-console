bbfc2aca27f9035ff8668ef15597a303a3f8a5bd
Nathanael Carauna <nathanael.carauna@gmail.com>
2021-06-01 17:41:38 -0300

    Bispo class implemented

---
+++ Xadrez-console/xadrez/Bispo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Bispo : Peca
+    {
+        public Bispo(Cor cor, Tabuleiro tabuleiro) : base(cor, tabuleiro)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+            
+            //NO
+            Posicao pos = new Posicao(0, 0);
+            pos.SetPosicao(Posicao.Linha + 1, Posicao.Coluna - 1);
+            while(Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if(Tabuleiro.Peca(pos)!=null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.SetPosicao(pos.Linha + 1, pos.Coluna - 1);
+            }
+
+            //NE
+            pos.SetPosicao(Posicao.Linha + 1, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.SetPosicao(pos.Linha + 1, pos.Coluna + 1);
+            }
+
+            //SE
+            pos.SetPosicao(Posicao.Linha - 1, Posicao.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.SetPosicao(pos.Linha - 1, pos.Coluna + 1);
+            }
+
+            //SO
+            pos.SetPosicao(Posicao.Linha - 1, Posicao.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.Peca(pos) != null && Tabuleiro.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.SetPosicao(pos.Linha - 1, pos.Coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
